76098a869b408c7936236f18e88740dc17e6a748
edward <edlnguye@ucsc.edu>
2015-01-03 13:21:55 -0800

    testing username and email

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -4,4 +4,50 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup 
+  	@user = User.new(name: "temp", email: "temp@mail.com")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+  	@user.name = "   "
+  	assert_not @user.valid?
+  end
+
+  test "email should be present" do
+  	@user.email = "   "
+  	assert_not @user.valid?
+  end
+
+  test "length of name should be no longer than 50 char" do
+  	@user.name = "a" * 51
+  	assert_not @user.valid?
+  end
+
+  test "length of email should be no longer than 50 char" do
+  	@user.email = "a" * 51
+  	assert_not @user.valid?
+  end
+
+  test "email: accepts valid emails" do
+  	valid_emails = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+  	valid_emails.each do |x|
+  		@user.email = x
+  		assert @user.valid?, "#{x.inspect} should be valid"
+  	end
+  end
+
+  test "email: doesn't accept valid emails" do
+  	valid_emails = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+  	valid_emails.each do |x|
+  		@user.email = x
+  		assert_not @user.valid?, "#{x.inspect} should be valid"
+  	end
+  end
 end
